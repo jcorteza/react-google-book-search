4433fa0862f5a372e75a195c922d3cc9ee36b04e
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-01-18 23:27:27 -0600

    created new, updated app build

---
+++ client/build/precache-manifest.8ca9396f80bfe49fe69fd3c7f9c6539c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f3857b6097ff666a0615ef6ad5a7902",
+    "url": "/static/media/books-2.4f3857b6.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "137c9b82feb9f131c32d",
+    "url": "/static/js/main.137c9b82.chunk.js"
+  },
+  {
+    "revision": "5e544d8a24c719eebe62",
+    "url": "/static/js/1.5e544d8a.chunk.js"
+  },
+  {
+    "revision": "137c9b82feb9f131c32d",
+    "url": "/static/css/main.fceeedc1.chunk.css"
+  },
+  {
+    "revision": "def3b1cf0d61d2132585886fc0c81be7",
+    "url": "/index.html"
+  }
+];
